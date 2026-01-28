31f70093da20c959f793239dabe672ffc1793978
Cmd69420 <inom6413@gmail.com>
2026-01-28 15:21:33 +0530

    Slotexpansionnmaingconvention

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -17,7 +17,7 @@ import BillingPlansPage from './BillingPlansPage';
 import BillingHistoryPage from './BillingHistoryPage';
 import PlanUsageWidget from './PlanUsageWidget';
 import MapViewerPage from './MapViewerPage';
-import SlotExpansionPage from "./Slotexpansionpage";
+import SlotExpansionPage from "./SlotexpansionPage";
 
 const API_BASE_URL = "https://backup-server-q2dc.onrender.com";
 
